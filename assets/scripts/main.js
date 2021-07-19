ba063f100092a7d8b9bf4a4d0864d5f12085e9ce
Lucas Vihuchi <lucasvihuchi@gmail.com>
2021-07-18 23:13:04 -0300

    Removed unused fonts and comments / Added comments on JS file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,6 @@
+// Change colors of clicked offer cards
 const offers = document.querySelectorAll('.offer');
+
 for (const offer of offers) {
     offer.addEventListener('click', function () {
         if (!(offer.classList.contains('offer-active'))) {
@@ -7,9 +9,11 @@ for (const offer of offers) {
     })
 }
 
+// Show/Hide back to top button according to scroll position 
 const homeBoundaries = document.getElementById('home').getBoundingClientRect();
 const backToTop = document.querySelector('.back-to-top');
-window.addEventListener('scroll' , function() {
+
+window.addEventListener('scroll', function () {
     if (window.pageYOffset >= Math.abs(homeBoundaries.top - homeBoundaries.bottom)) {
         backToTop.classList.remove('back-to-top-hidden');
     }
@@ -18,13 +22,15 @@ window.addEventListener('scroll' , function() {
     }
 })
 
+
 const inputName = document.getElementById('client-name');
 const inputEmail = document.getElementById('client-email');
 const newsletterForm = document.getElementById('newsletter-form');
 const modal = document.querySelector('.modal');
 const modalNameField = document.querySelector('.modal-name');
 
-newsletterForm.onsubmit = function(e) {
+// Prevent form from refreshing the screen + Show modal with client's name
+newsletterForm.onsubmit = function (e) {
     e.preventDefault();
     if (inputName.value.length > 3 && inputEmail.value.length > 3) {
         modalNameField.innerText = localStorage.getItem('name').split(' ')[0];
@@ -32,14 +38,16 @@ newsletterForm.onsubmit = function(e) {
     }
 };
 
-function signupConfirm () {
+// Save client's name and email on local storage
+function signupConfirm() {
     if (inputName.value.length > 3 && inputEmail.value.length > 3) {
         localStorage.setItem('name', inputName.value);
         localStorage.setItem('email', inputEmail.value);
     }
 }
 
-modal.addEventListener('click', function() {
+// Hide modal when it's on screen by clicking on it + Clean input fields
+modal.addEventListener('click', function () {
     modal.classList.add('modal-hidden');
     inputName.value = '';
     inputEmail.value = '';
